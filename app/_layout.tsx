8d88427d2db4956611a221c01f6b7427cf0ab48e
Kevin Chen <Xuhua1709@hotmail.com>
2025-07-01 00:26:26 +0200

    Inserted fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,25 @@
+import { useFonts } from 'expo-font';
 import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    'HelloHeadlineW00Regular': require('../assets/fonts/HelloHeadlineW00Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return <Stack />;
 }
+
